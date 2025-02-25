73c4ddb71105a42e720ff81d2d9d95dfd8aba854
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2025-02-25 13:43:31 +0100

    added reverse text

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-import {add, subtract}from "./stringUnits.ts"
+import path from "path";
+import {add, subtract, reverse}from "./stringUnits.ts"
+import fs from "fs";
 
 console.log(add(5, 3))
 console.log(subtract(5, 2))
-console.log()
+const sentence = ("its better to know node then to not know it");
+
+const reversedSentence = reverse(sentence);
+fs.writeFileSync('reversed.txt', reversedSentence);
+
+const filePath = path.join(__dirname, 'reversed.txt');
+console.log(`Reversed sentence saved at: ${filePath}`);
+
+
+function reverseString (str: string): string {
+return str.split("").reverse().join("");
+}
+const textRevers =
+reverseString ("its good to learn node to be able to code more effectivly and fast when you get it down and know how to use it");
+console.log(reverseString);
--- stringUnits.ts
+++ stringUnits.ts
@@ -1,5 +1,8 @@
 export const add =(a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
-export const reverse = (a: number, b: number) => a - b;
-export const join = (a: number, b: number) => a - b;
-export const split = (a: number, b: number) => a - b;
+export const reverse = (str: string): string => {
+    return str.split('').reverse().join('');
+};
+export const countCharacters = (str: string): number => {
+    return str.length;
+};
